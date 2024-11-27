394f9bef93918ad01e5ccbd59318e552e6e7a053
K1ssSh0t <angelomarmatias@gmail.com>
2024-11-26 19:50:15 -0600

    feat(services): agregado la funcion para saver si un usuario es Operador

--- services/authorization-service.ts
+++ services/authorization-service.ts
@@ -3,3 +3,7 @@ import { Session } from "next-auth";
 export function isAdmin(session: Session) {
   return session?.user?.role === "admin";
 }
+
+export function isOperador(session: Session) {
+  return session?.user?.role === "operador";
+}
